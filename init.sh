30ef670615d9cb04173d0a616661d0a518d2f348
Kaesi Manakkal <kaesimanakkal@gmail.com>
2023-04-25 15:47:37 -0400

    save firebase cred before starting redis and celery

--- init.sh
+++ init.sh
@@ -2,17 +2,17 @@
 # https://github.com/coqui-ai/TTS --> Text to Speech
 # https://github.com/alphacep/vosk-api --> Speech to Text
 
-# start message queue (redis)
-service redis-server start
-
-# start backend (celery) in background
-celery -A app.celery worker --loglevel=info --pidfile=/tmp/celery-worker.pid -f /tmp/celery-worker.log -D
-
 # save firebase cred
 mkdir -p private
 echo "$FIREBASE_CRED" | base64 -d > private/firebase_cred.json
 
 mkdir -p upload/audio results static
+
+# start message queue (redis)
+service redis-server start
+
+# start backend (celery) in background
+celery -A app.celery worker --loglevel=info --pidfile=/tmp/celery-worker.pid -f /tmp/celery-worker.log -D
 
 # start frontend (flask)
 gunicorn -p /tmp/gunicorn.pid --bind ${HOST}:${PORT} wsgi:app
